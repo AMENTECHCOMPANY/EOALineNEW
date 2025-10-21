0547c7a849fa0835e0c7f1c54cecd694bef38a2c
AMENTECHCOMPANY <amentech.company@gmail.com>
2025-10-21 10:25:48 +0200

    Delete src directory

--- src/components/InputFieldBlurHandler.sh
+++
@@ -1,1 +0,0 @@
-git push origin maingit add src/components/Hero.tsx
--- src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    base: "./", // important for Netlify deployment
-    plugins: [react()],
-});
